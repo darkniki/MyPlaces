34640333ce645f406e212193526fda47fe1d31e5
Nikita Lapsar <darkniki777@gmail.com>
2021-03-01 11:01:40 +0300

    20

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Nikita Lapsar on 25.02.2021.
+//
+
+import RealmSwift
+
+class Place: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
+    
+    convenience init(name: String, location: String?, type: String?, imageData: Data?) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
+    }
+}
